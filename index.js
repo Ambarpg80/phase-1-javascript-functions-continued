d017aba805fcb0d06e90c116aac60ba4c5dff649
Ambar <Ambarpg80@gmail.com>
2022-09-04 11:55:30 -0400

    commit tree 2b25a3cb9e712ec6c2f4c2009e171cad8b155582

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // code your solution here
+function saturdayFun(item){
+    if (item){
+      return (`This Saturday, I want to ${item}!`)  
+    }
+    else {
+        return (`This Saturday, I want to roller-skate!`)
+    }
+}
+const mondayWork = function(item){
+    if (item){
+      return (`This Monday, I will ${item}.`);  
+    }
+    else {
+        return (`This Monday, I will go to the office.`);
+    }
+}
+mondayWork();
+/*
+function outer(greeting, msg = "It's a fine day to learn") {
+  // 2
+  const innerFunction = function (name, lang = "Python") {
+    // 3
+    return `${greeting}, ${name}! ${msg} ${lang}`; // 4
+  };
+  return innerFunction("student", "JavaScript"); // 5
+}
+
+outer("Hello");
+
+let result = wrapAdjective("||")
+let emphatic = result("a dedicated programmer")
+
+*/
+function wrapAdjective(special = "*" ) {
+    // 2
+    const result = function (msg ) {
+      // 3
+      return `You are ${special}${msg}${special}!`; // 4
+    };
+    return result; // 5
+  }
+  
+
+wrapAdjective("%")("a dedicated programmer");
+wrapAdjective("||")("a dedicated programmer");
